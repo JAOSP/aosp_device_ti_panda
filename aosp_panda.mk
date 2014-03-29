5e00d71ce19082b7792f9dc1913b42080bd1c0a7
Akihiro Maeda <sola.1980.a@gmail.com>
2014-03-30 02:13:22 +0900

    Set to build aosp and full
    full: aosp + gapps
    aosp: only aosp

--- aosp_panda.mk
+++ aosp_panda.mk
@@ -13,6 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-$(call inherit-product, device/ti/panda/full_panda.mk)
+
+# Live Wallpapers
+PRODUCT_PACKAGES += \
+        LiveWallpapers \
+        LiveWallpapersPicker \
+        MagicSmokeWallpapers \
+        VisualizationWallpapers \
+        librs_jni
+
+PRODUCT_PROPERTY_OVERRIDES := \
+        net.dns1=8.8.8.8 \
+        net.dns2=8.8.4.4
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product, device/ti/panda/device.mk)
+$(call inherit-product-if-exists, vendor/ti/proprietary/omap4xxx/ti-omap4-vendor.mk)
 
 PRODUCT_NAME := aosp_panda
+PRODUCT_DEVICE := panda
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := AOSP on Panda
+PRODUCT_MANUFACTURER := Texas_Instruments_Inc
+
+#AOSP
+$(call inherit-product, device/aosp/common/aosp.mk)
--- full_panda.mk
+++ full_panda.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2011 The Android Open Source Project
+#
+# Copyright 2013 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,14 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-#
-# This file is the build configuration for a full Android
-# build for toro hardware. This cleanly combines a set of
-# device-specific aspects (drivers) with a device-agnostic
-# product configuration (apps). Except for a few implementation
-# details, it only fundamentally contains two inherit-product
-# lines, full and toro, hence its name.
 #
 
 # Live Wallpapers
@@ -43,3 +36,6 @@ PRODUCT_DEVICE := panda
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := AOSP on Panda
 PRODUCT_MANUFACTURER := Texas_Instruments_Inc
+
+#AOSP
+$(call inherit-product, device/aosp/common/full.mk)
